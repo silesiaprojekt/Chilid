613fe4e0e32d3595883dfc9498a9aa738501d729
Unosardinia <platformarobocza@gmail.com>
2016-08-08 19:20:22 +0200

    Final

--- js/header.js
+++ js/header.js
@@ -1,5 +1,6 @@
+$(document).ready(function(){
 
-
+//smoth scroll to hash
 $(function() {
   $('a[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
@@ -7,51 +8,53 @@ $(function() {
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
         $('html, body').animate({
-          scrollTop: target.offset().top
+          scrollTop: target.offset().top - 110
         }, 1000);
         return false;
       }
     }
   });
 
-
-   $(function(){
-       $(".fot1 img").mouseenter(function(){
-           $(this).toggleClass("test");
-       });
-   });
-
-
-
-
-
+//show and hide box names
+$("#box1").show();
   $(function() {
-    $(".button").click(function() {
-        var selector = this.id.substring(this.id.indexOf("_") + 1);
-        alert(selector)
-        $("#" + selector).fadeIn().siblings("#" + selector).fadeOut();
-    });
+$(".data").click(function(){
+    var id = $(this).data("id");
+  $(".hide").hide();
+  $("#"+id).show();
 });
+  });
 
 
+//slider
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function slideIt() {
+  $('#slideshow > div:first')
+    .slideToggle(1000)
+    .next()
+    .slideToggle(1000)
+    .end()
+    .appendTo('#slideshow');
+},  4000);
 
 
-
+//sticky header
 $(window).scroll(function() {
-if ($(this).scrollTop() > 600){
-
+    var $head = $("#top").outerHeight();
+    console.log($head);
+    if ($(this).scrollTop() > $head){
     $("header").addClass("header-fixed");
-
   }
   else{
-
     $("header").removeClass("header-fixed");
-
   }
 });
-
-
 });
 
+//mobile nav show and hide
+$(".mobile-nav").click(function(){
+   $(".mobile-layer").slideToggle("display-it");
+});
 
-
+})
